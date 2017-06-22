4dfafdfd26732d69f3d0b581d50d345ce0ccac69
Atul Gaikwad <atulgaikwad91@gmail.com>
2017-06-22 11:43:32 +0530

    simple mongodb app

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect('mongodb://demo23:demo1234@ds133932.mlab.com:33932/demodb', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+   if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
